2d1a49f31d3d09da101e4bec63b1acb08031399a
Animesh <animeshbhople885507@gmail.com>
2025-10-31 16:47:03 +0530

    update Testomnials

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -92,28 +92,32 @@ const TestimonialsColumn = (props: {
       }}
       className="flex flex-col gap-6 pb-6"
     >
-      {[...new Array(2)].fill(0).map((_, index) => (
-        <React.Fragment key={index}>
-          {props.testimonials.map(({ text, imageSrc, name, username }) => (
-            <div className="card">
-              <div>{text}</div>
-              <div className="flex items-center gap-2 mt-5">
-                <Image
-                  src={imageSrc}
-                  alt={name}
-                  width={40}
-                  height={40}
-                  className="h-10 w-10 rounded-full"
-                />
-                <div className="flex flex-col">
-                  <div className="font-medium tracking-tight leading-5">
-                    {name}
+      {[...new Array(2)].fill(0).map((_, outerIndex) => (
+        <React.Fragment key={outerIndex}>
+          {props.testimonials.map(
+            ({ text, imageSrc, name, username }, innerIndex) => (
+              <div key={innerIndex} className="card">
+                <div>{text}</div>
+
+                <div className="flex items-center gap-2 mt-5">
+                  <Image
+                    src={imageSrc}
+                    alt={name}
+                    width={40}
+                    height={40}
+                    className="h-10 w-10 rounded-full"
+                  />
+
+                  <div className="flex flex-col">
+                    <div className="font-medium tracking-tight leading-5">
+                      {name}
+                    </div>
+                    <div className="leading-5 tracking-tight">{username}</div>
                   </div>
-                  <div className="leading-5 tracking-tight">{username}</div>
                 </div>
               </div>
-            </div>
-          ))}
+            )
+          )}
         </React.Fragment>
       ))}
     </motion.div>
@@ -128,19 +132,24 @@ export const Testimonials = () => {
           <div className="flex justify-center">
             <div className="tag">Testimonials</div>
           </div>
+
           <h2 className="section-title mt-5">What our users say</h2>
+
           <p className="section-description mt-5">
             From intuitive design to powerful features, our app has become an
             essential tool for users around the world.
           </p>
         </div>
+
         <div className="flex justify-center gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)] max-h-[738px] overflow-hidden">
           <TestimonialsColumn testimonials={firstColumn} duration={15} />
+
           <TestimonialsColumn
             testimonials={secondColumn}
             className="hidden md:block"
             duration={19}
           />
+
           <TestimonialsColumn
             testimonials={thirdColumn}
             className="hidden lg:block"
